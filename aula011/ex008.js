4087235d998b8fd6e44121aeef0a20dfc76c1aeb
rafaeltuao <rafaelhenriquetuaosardela@gmail.com>
2022-06-09 18:32:47 -0300

    exercícios concluídos

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre com cinto de segurança`)
