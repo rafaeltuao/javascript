60b0d120df80a3079e717632dbf79bf3da249114
rafaeltuao <rafaelhenriquetuaosardela@gmail.com>
2022-06-24 17:27:30 -0300

    terminei os 2 exercicios

---
+++ aula013/aula13ex/script.js
@@ -0,0 +1,40 @@
+function Verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[erro] verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByClassName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if(idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = "Mulher"
+            if(idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+        }
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
