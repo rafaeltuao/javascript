2841b09a4750b6824c1877a4b3c8b0930e7dd1e2
rafaeltuao <rafaelhenriquetuaosardela@gmail.com>
2022-06-28 21:40:56 -0300

    terminei o ex da aula 16

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -5,13 +5,17 @@ function contar() {
     let res = document.getElementById('res')
     
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        Window.alert('[ERRO] Faltam dados!')
+        window.alert('[ERRO] Faltam dados!')
         res.innerHTML = 'Impossível contar'
     } else {
-        res.innerHTML = 'Contando'
+        res.innerHTML = 'Contando <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo invalido! Considerando PASSO 1')
+            p = 1
+        }
         if (i < f) {
             // Contagem crescente
             for(let c = i;c <= f;c += p ) {
